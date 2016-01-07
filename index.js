aa29f85686ff897494dd42223950f4b0d28c6ee6
Max Novak <novak.max@gmail.com>
2016-01-07 23:15:22 +0000

    Cleans up code
    Used auto formatter and removed some unneed
    console logs.

--- index.js
+++ index.js
@@ -13,25 +13,29 @@ var MatchData = require('./MatchData_model.js');
 mongoose.connect(mongoUri);
 
 app.set('port', process.env.PORT || 3000);
-app.use(bodyParser.urlencoded({ extended : false}));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, '/')));
 
-app.get('/getGames', function (req, res) {
-    MatchData.find().distinct("GameName", function (error, data) {
+app.get('/getGames', function(req, res) {
+    MatchData.find().distinct("GameName", function(error, data) {
         console.log(data);
         res.send(data);
     });
 });
 
-app.post('/data', function (req, res) {
+app.post('/data', function(req, res) {
     var game = req.body.game;
-    MatchData.find({"GameName": game}, function (err, data) {
+    MatchData.find({
+        "GameName": game
+    }, function(err, data) {
         if (err) return console.error(err);
         res.send(data);
     });
 });
 
-app.post('/submitWinLoss', function (req, res) {
+app.post('/submitWinLoss', function(req, res) {
     var gameName = req.body.GameName;
     var winner = req.body.Winner;
     var loser = req.body.Loser;
@@ -40,11 +44,12 @@ app.post('/submitWinLoss', function (req, res) {
     console.log("Game Name: " + gameName);
     console.log("Winner: " + winner);
     console.log("List of losers: " + listOfLosers);
-    
-    mongoose.createConnection(mongoUri, function (err, res) {
+
+    mongoose.createConnection(mongoUri, function(err, res) {
         if (err) {
             console.error('Error connecting: ' + err);
-        } else {
+        }
+        else {
             storeResults(winner, "win", gameName);
             for (var i = 0; i < listOfLosers.length; i++) {
                 storeResults(listOfLosers[i], "loss", gameName);
@@ -52,61 +57,61 @@ app.post('/submitWinLoss', function (req, res) {
         }
     });
     res.send("success");
-    
+
 });
 
 
-http.createServer(app).listen(app.get('port'), function () {
+http.createServer(app).listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
 function storeResults(player, result, game) {
-   MatchData.find({"Player":player}).limit(1).count(function(err, data){
-        if(err){
+    MatchData.find({
+        "Player": player
+    }).limit(1).count(function(err, data) {
+        if (err) {
             console.error(err);
         }
-        if (data < 1){
-            
-        
-            var matchData = new MatchData({ 
-                                   "Player" : player,
-                                   "GameList" : [{
-                                       "GameName" : game,
-                                       "Outcome" : result
-                                   }]
-                                });
-            
-            matchData.save(function (err, matchData) {
+        if (data < 1) {
+
+            var matchData = new MatchData({
+                "Player": player,
+                "GameList": [{
+                    "GameName": game,
+                    "Outcome": result
+                }]
+            });
+
+            matchData.save(function(err, result) {
                 if (err) {
                     return console.error(err);
                 }
-                console.log(matchData);
+                console.log(result);
             });
-        }else{
-        
-    
-             MatchData.find({"Player":player}, function(err, data) {
-                
-                
-                if (err){
+        }
+        else {
+            
+            MatchData.find({
+                "Player": player
+            }, function(err, data) {
+
+                if (err) {
                     return console.error(err);
                 }
-                console.log(data[0]);
-                console.log("            ");
-                console.log(data[0].GameList);
                 var newGame = {
                     GameName: game,
                     Outcome: result
                 };
-                
+
                 data[0].GameList.push(newGame);
-                
-                data[0].save(function(err, moreData){
-                   if(err){
-                       return console.error(err);
-                   } 
+
+                data[0].save(function(err, result) {
+                    if (err) {
+                        return console.error(err);
+                    }
+                    console.log(result)
                 });
             });
-        } 
+        }
     });
 }
